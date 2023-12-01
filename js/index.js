ff46abbafe3912b0da7cd30316859990d8c9add3
feruzbekqahramov <feqahramnov@mail.com>
2023-12-01 20:25:53 +0500

    Homework finish

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let sum = +prompt('9 732 742,6 so`m bor!');
+console.log(sum);
+let d = confirm('750$, 120€ dan so`mga o`tkazildi');
+console.log(d);
+let one = 500*11000.34;
+let twoo = 120*12354.03;
+let three = 250*11000.34;
+let mone = one+twoo+three;
+let money = prompt("Jamg'armangizni kiriting");
+let res;
+if (mone - money >= 0){
+    res = "Uyingizda damizi oling"
+} else{
+    res = "Oq yo'l"
+}
+alert(res);
